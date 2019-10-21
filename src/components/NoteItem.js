87e4a2549b677d41dd3fc831bd50d05acf5438d1
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-10-21 14:43:19 -0400

    Note bodies are truncated

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Truncate from 'react-truncate';
 
 const NoteItem = (props) => {
   // console.log(props)
   return(
+
   <li onClick={() => props.handleNoteViewer(props.note)}>
+
     <h2>{props.note.title}</h2>
+    <Truncate>
     <p>{props.note.body}</p>
+    </Truncate>
   </li>
+
  );
 }
 
--- src/components/NoteViewer.js
+++ src/components/NoteViewer.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 
+
 const NoteViewer = (props) => {
   return (
     <Fragment>
